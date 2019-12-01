baa4cd7c31aaa9bd727f478d09275138cf0b37d7
ivanfslee <ivanfslee@gmail.com>
2019-12-01 15:58:44 -0800

    Added user search term submission

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        //event obj parameter 
-        console.log(event.target.value); //contains search string user typed into search bar
+    state = { term: '' };
+
+    onFormSubmit= event => {
+        event.preventDefault(); //stops browser from refreshing the page
+        //console.log(this.state.term);
+        this.props.runWhenUserSubmits(this.state.term);
     }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                     <label>Image Search</label>
-                        {/* any time input in search bar, onInputChange method will be invoked. onChange property */}
-                        <input type="text" onChange={this.onInputChange} />
+                        <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
             </div>
